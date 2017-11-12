566e5b0c4828bb1aa8ecfbaa58640453407083a4
KiYeongKing <walkst26@gmail.com>
2017-11-12 20:29:45 +0900

    Revert "Ki"

---
+++ public/Js/Map.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 순우 on 2017-11-07.
+ */
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var myCenter = new google.maps.LatLng(36.6251, 127.45718299999999);
+    var marker = new google.maps.Marker({position:myCenter});
+    var mapOptions = {
+        center: myCenter,
+        zoom: 14
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    marker.setMap(map);
+    google.maps.event.addListener(marker,'click',function() {
+        map.setZoom(17);
+        map.setCenter(marker.getPosition());
+    });
+}
