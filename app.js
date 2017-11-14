bbafcdfe6b1d7e7c5d188b0310158b1a71309d02
SehyeonGil <rlftpgus@naver.com>
2017-11-14 21:04:19 +0900

    app.js fix

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var moment = require('moment');
+var session=require('express-session');
+var mongoose=require('mongoose');
+var passport = require('passport');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+mongoose.connect('mongodb://localhost/FooDIY');
+mongoose.Promise = global.Promise;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(session({
+    secret: '123456789!@#$',
+    resave: false,
+    saveUninitialized: true
+}));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
---
+++ models/message.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Sehyeon on 2017-08-10.
+ */
+var mongoose = require('mongoose'),
+    bcrypt = require('bcrypt-nodejs'),
+    moment = require('moment'),
+    autoIncrement = require('mongoose-auto-increment');
+
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+    conver_id:{type:String,required: true},
+    from:{type:String,required: true},
+    to:{type:String,required: true},
+    content:{type:String, required:true},
+    checked:Boolean,
+    time_created:{type:Date}
+    // time_created:{type:Date, default:moment().format()}
+});
+module.exports = mongoose.model('message', MessageSchema);
