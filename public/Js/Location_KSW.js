edf227a263c04dd1f33a6b95c076ae54b71ee317
ksw <artbit2012@gmail.com>
2017-11-27 21:59:21 +0900

    test

---
+++ public/Js/Location_KSW.js
@@ -0,0 +1,15 @@
+/**
+ * Created by 순우 on 2017-11-27.
+ */
+
+function goToMenuInfo() {
+    location.href = "../html/MenuInfo_KSW.html";
+}
+
+function goToHome() {
+    location.href = "../html/Home_KSW.html";
+}
+
+function goToMenuList() {
+    location.href = "MenuList_KSW.html";
+}
